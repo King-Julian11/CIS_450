0083c3d6b4bed8055b23659aa5c1e3c98b94124a
King-Julian11 <blazerman741@gmail.com>
2023-04-04 16:07:03 -0500

    Object pooling

---
+++ Assignment 10/Assets/Scripts/Object Pool Scripts/Pool.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool
+{
+    public string tag;
+    public GameObject prefab;
+    public int size;
+}
